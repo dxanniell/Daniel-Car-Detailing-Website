d7c4050e366ecaf4b844f1c183da8b33f4bffc9a
dxanniell <144173285+dxanniell@users.noreply.github.com>
2023-12-12 17:08:46 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -10,6 +10,19 @@ const person = {
   favoriteColor:"Green"
   age:"20"
 };
+let nameOfElement;
+
+let date1 = new Date("9/11/23");
+let date2 = new Date();
+
+// To calculate the time difference of two dates
+let Difference_In_Time = date2.getTime() - date1.getTime();
+
+// To calculate the no. of days between two dates
+let Difference_In_Days = 
+	Math.round(Difference_In_Time / (1000 * 3600 * 24));
+
+
 // To display the final no. of days (result)
 
 function Days() {
