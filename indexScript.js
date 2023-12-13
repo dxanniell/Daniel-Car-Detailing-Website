2e1f7effe24fa0859215b8ffac8abcd05a741c13
dxanniell <144173285+dxanniell@users.noreply.github.com>
2023-12-13 09:46:25 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,18 +1,5 @@
-let nameOfElement;
-
-let date1 = new Date("9/11/23");
-let date2 = new Date();
-
-// To calculate the time difference of two dates
-let Difference_In_Time = date2.getTime() - date1.getTime();
-
-// To calculate the no. of days between two dates
-let Difference_In_Days = 
-	Math.round(Difference_In_Time / (1000 * 3600 * 24));function changeText(){
-  document.getElementById("demo").innerHTML="Hi"
-}
-function printToConsole(input) {
-    window.alert(input);
+function changetext(){
+document.getElementById("demo").innerHTML="Hi"
 }
 let person = {
   firstName:"Daniel",
@@ -22,10 +9,4 @@ let person = {
 function birthday(){
 	person.age=person.age+1;
 	window.alert(person.age);
-}
-// To display the final no. of days (result)
-
-function Days() {
-// To display the final no. of days (result)
-window.alert ("It's been " +  Difference_In_Days + " days");
 }
